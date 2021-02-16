015249898417d7400eed11e9cf843880e06f9a4b
Rami-hlioui <ramihlioui.tsuna@gmail.com>
2021-02-16 20:25:35 +0100

    first node steps

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+let products = [];
+
+app.use(express.json());
+/*app.use("/", (req, res) => res.send("Hello world!"));*/
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.post("/products", (req, res) => {
+  const body = req.body;
+  products.push(body);
+  res.json(body);
+});
+
+app.delete("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const filtredProducts = products.filter((value, index) => index != id);
+  products = filtredProducts;
+  res.json(filtredProducts);
+});
+app.patch("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  products[id] = body;
+  res.json(products);
+});
+
+module.exports = app;
